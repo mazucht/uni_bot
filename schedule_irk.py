bf58844d9be2b6f9d57d9fc37d832e1a17c6a39b
mazucht <toleg9410@gmail.com>
2026-09-19 01:51:23 +0800

    Доработка поиска информации по расписания ИрНИТУ.

--- schedule_irk.py
+++ schedule_irk.py
@@ -1,6 +1,7 @@
 import requests as r
 from bs4 import BeautifulSoup
 
+# все лекций и их классы
 types = {
            'schcls-item-distype type-1' : 'лекция',
            'schcls-item-distype type-2' : 'практика',
@@ -9,27 +10,37 @@ types = {
 
 class ScheduleParser():
     def __init__(self):
-        
+        # инструменты поиска
         site = r.get('https://www.istu.edu/raspisanie/grup/478232')
-
         finder = BeautifulSoup(site.text, 'html.parser')
+
+        # информация о всех днях рабочей недели
         days = finder.find_all('div', 'sch-list-day')
 
+        # создание основного словаря для хранения всех данных
         self.schedule = {}
-        
+
+        # перебор информации о каждом рабочем дне
         for day in days:
+            # какой рабочий день обрабатывается
             day_of_week = day.find('h2', 'sch-list-day-header').text.split(', ')[0]
-            #print(day_of_week)
+
+            # организация места хранения информации о рабочем дне
             self.schedule[day_of_week] = {}
-            
+
+            # поиск всей информации по парам
             subjects = day.find_all('div', 'sch-list-item')
+
+            # перебор информации по каждому предмету
             for sub in subjects:
+                # извлечение необходимой информации
                 time = sub.find('div', 'sch-list-item-time-inner').text
                 name = sub.find('div', 'schcls-item-name').text
                 teacher = sub.find('div', 'schcls-item-prepod').text
                 group = sub.find('div', 'schcls-item-group').text
                 place = sub.find('div', 'schcls-item-aud').text
 
+                # определение типа пары
                 lesson_type = 'Отсутсвует'
 
                 for t in types.keys():
@@ -38,16 +49,13 @@ class ScheduleParser():
 
                     lesson_type = types[t]
 
+                # оргаенизвция места хранения инормации о паре
                 self.schedule[day_of_week][name] = {}
+
+                # запись данных о паре
                 self.schedule[day_of_week][name]['name'] = name
                 self.schedule[day_of_week][name]['time'] = time
                 self.schedule[day_of_week][name]['type'] = lesson_type
                 self.schedule[day_of_week][name]['teacher'] = teacher
                 self.schedule[day_of_week][name]['group'] = group
                 self.schedule[day_of_week][name]['place'] = place
-
-                #print(self.schedule[day_of_week][name])
-
-sch = ScheduleParser()
-
-print(sch.schedule['Пятница'].keys())
